adb8856741724d9543d800b10b8596ebb00aea23
Douglas-Dorug <douglasx30@gmail.com>
2021-08-19 17:11:45 -0300

    Desenvolvidos formularios e templates para o projeto. Desabilitado strict null checks em tsconfig.json

--- src/app/courses/course.ts
+++ src/app/courses/course.ts
@@ -1,12 +1,12 @@
 export class Course{
-    id!: number;
-    name!: string;
-    imageUrl!: string;
-    price!: number;
-    code!: string;
-    duration!: number;
-    rating!: number;
-    releaseDate!: string;
-    description!: string;
+    id: number;
+    name: string;
+    imageUrl: string;
+    price: number;
+    code: string;
+    duration: number;
+    rating: number;
+    releaseDate: string;
+    description: string;
     
 }
